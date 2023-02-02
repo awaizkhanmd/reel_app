07972871891b9f9d3a65380927c8e8d5e7d76319
MD AWAIZ <awaizkhanmd@gmail.com>
2023-02-03 02:00:12 +0530

    install of sanit ()

---
+++ sanity-backend/sanity.cli.ts
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'zk7v62zr',
+    dataset: 'production'
+  }
+})
---
+++ sanity-backend/schemas/index.ts
@@ -0,0 +1,5 @@
+import user from './user'
+import comment from './comment'
+import postedBy from './postedBy'
+import post from './post'
+export const schemaTypes = [user,comment, postedBy,post]
